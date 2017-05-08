710c1ab3f71963daeb62a5c34106e17557b462ea
Erik Rose <erik@mozilla.com>
2017-05-08 13:46:26 -0400

    Dev panel shows but duplicates itself when focused.

---
+++ examples/fathom_debugger/README.md
@@ -0,0 +1,5 @@
+# Fathom Debugger
+
+## What it does
+
+This runs your Fathom ruleset against the current tab and shows you the result.
---
+++ examples/fathom_debugger/devtools.js
@@ -0,0 +1,9 @@
+browser.devtools.panels.create(
+  "Fathom",                      // title
+  "icon.png",                // icon
+  "devtools.html"      // content
+);
+// .then((newPanel) => {
+//   newPanel.onShown.addListener(initialisePanel);
+//   newPanel.onHidden.addListener(unInitialisePanel);
+// });
